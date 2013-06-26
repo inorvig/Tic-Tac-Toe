b53bf4be0073bca20db4f9e7e566a1bb13c4adb5
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-26 17:24:47 -0400

    Use new reset() to reset, rather than non-existent restart()

--- tictactoe.js
+++ tictactoe.js
@@ -44,7 +44,7 @@
         document.getElementById("turnlabel").style.display = "none";
         document.getElementById("alone").style.display = "none";
         document.getElementById("together").style.display = "inline";
-        restart();
+        reset();
     };
 
     //Turns off the AI, sets the text of the labels relating to turn and player names, and resets the game
