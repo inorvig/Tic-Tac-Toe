336b78f0e13285432d1249cbc00ec77e0c13e3f9
bellarose94 <bellbell@gmail.com>
2013-06-27 15:33:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,13 @@ var express = require('express')
   
   var app = express();
 
-  /* Express middleware */
-
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/public');
   app.use(express.static('public'));
 
-  /* Our lone route */
-
   app.get('/', function(req, res) {
     res.sendfile(__dirname+'/public/tictac_html.html');
   });
-
-  /* Let there be life! */
 
   http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port') + '.');
